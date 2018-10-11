2ad6892c58884f26a6a2330eaa17c0d9f6c8127b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-11 20:28:23 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,7 +2,7 @@ require 'pry'
 # code here!
 
 class School
-  attr_accessor :roster
+  attr_accessor :roster :names
 
   def initialize(roster)
     @roster = {}
@@ -10,22 +10,28 @@ class School
   
   def add_student(name, grade)
     if @roster.include?(grade)
-      @roster[grade] << name
+      @roster[grade][names] << name
     else
-      @roster[grade] = []
-      @roster[grade] << name
+      @roster[grade][names] = {}
+      @roster[grade[names] << name
     end
   end
 
   def grade(grade)
-    return @roster[grade]    
+    return @roster[grade][names]   
   end
   
   def sort
+<<<<<<< HEAD
     @roster.each do |key, values|
 # binding.pry
       values.sort!
     end 
+=======
+   @roster.keys.sort
+   @roster.values.sort
+   binding.pry
+>>>>>>> a4a86d410082f1dda0275f59d37385f0e22a9339
   end
 
 end
